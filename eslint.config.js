7b1176a286635fa15e2e21ff425e0dea7018ba22
Nikita Shirobokov <ns.shirobokov@gmail.com>
2025-03-05 14:31:52 +0300

    refactor: deps upgrade
    * Upgrades remix-auth, remix-auth-oauth2 packages
    * Removes remix v1 dependencies

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        {
+          accessibility: 'explicit',
+          overrides: {
+            accessors: 'explicit',
+            constructors: 'no-public',
+            methods: 'explicit',
+            properties: 'explicit',
+            parameterProperties: 'explicit',
+          },
+        },
+      ],
+      '@typescript-eslint/no-throw-literal': 'off',
+      'sonarjs/no-duplicate-string': 'off',
+      'sonarjs/elseif-without-else': 'off',
+    },
+  },
+]
